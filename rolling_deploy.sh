0cb7e3163e6ca20a64aef0c56e1cda5f6d37d3ae
toboid <toboid@users.noreply.github.com>
2015-10-07 16:25:44 +0100

    Reuse image name

--- rolling_deploy.sh
+++ rolling_deploy.sh
@@ -16,12 +16,13 @@ NAMESPACE=$3
 
 VERSION_ID=${CIRCLE_SHA1:0:7}
 REMOTE_REPOSITORY=${GCLOUD_REGISTRY_PREFIX}gcr.io/${CLOUDSDK_CORE_PROJECT}
+QUALIFIED_IMAGE_NAME=${REMOTE_REPOSITORY}/${IMAGE}:${VERSION_ID}
 
 export CLOUDSDK_CORE_DISABLE_PROMPTS=1
 export CLOUDSDK_PYTHON_SITEPACKAGES=1
 
 # Build image
-docker build -t ${REMOTE_REPOSITORY}/${IMAGE}:${VERSION_ID} .
+docker build -t ${QUALIFIED_IMAGE_NAME} .
 
 # Authenticate gcloud SDK
 echo $GCLOUD_KEY | base64 --decode > gcloud.p12
@@ -32,8 +33,8 @@ ssh-keygen -f ~/.ssh/google_compute_engine -N ""
 ~/google-cloud-sdk/bin/gcloud container clusters get-credentials $GCLOUD_CLUSTER
 
 # Push image to gcloud
-~/google-cloud-sdk/bin/gcloud docker push ${REMOTE_REPOSITORY}/${IMAGE}:${VERSION_ID} > /dev/null
+~/google-cloud-sdk/bin/gcloud docker push ${QUALIFIED_IMAGE_NAME} > /dev/null
 
 # Rolling update
 OLD_RC=$(~/google-cloud-sdk/bin/kubectl get rc -l ${SELECTOR} --namespace=${NAMESPACE} | cut -f1 -d " " | tail -1)
-~/google-cloud-sdk/bin/kubectl rolling-update ${OLD_RC} ${IMAGE}-rc-${VERSION_ID} --image=${REMOTE_REPOSITORY}/${IMAGE}:${VERSION_ID} --namespace=${NAMESPACE}
+~/google-cloud-sdk/bin/kubectl rolling-update ${OLD_RC} ${IMAGE}-rc-${VERSION_ID} --image=${QUALIFIED_IMAGE_NAME} --namespace=${NAMESPACE}
