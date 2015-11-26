42cf0077bbaa5fecd893d700a3259662398e6af0
Chris Impey <chris@bigwednesday.io>
2015-11-26 13:13:05 +0000

    split existing scripts so authentication can be reused

---
+++ authenticate.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+set -e
+
+echo "Activating gcloud service account"
+
+echo $GCLOUD_KEY | base64 --decode > gcloud.p12
+
+gcloud auth activate-service-account $GCLOUD_EMAIL --key-file gcloud.p12
+
+ssh-keygen -f ~/.ssh/google_compute_engine -N ""
+
+gcloud docker -a
